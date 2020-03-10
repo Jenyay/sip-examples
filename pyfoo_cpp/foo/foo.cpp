9cd34e4ea39c7b45fde9bbf5a54fe3ba4ca6dd09
Eugene Ilin <jenyay.ilin@gmail.com>
2020-03-10 10:09:00 +0300

    Add C++ example

--- pyfoo_cpp/foo/foo.cpp
+++ pyfoo_cpp/foo/foo.cpp
@@ -1,10 +1,18 @@
+#include <string.h>
+
 #include "foo.h"
 
-using std::string;
+
+Foo::Foo(int int_val, char* string_val): _int_val(int_val) {
+	_string_val = nullptr;
+	set_string_val(string_val);
+}
 
 
-Foo::Foo(int int_val, string string_val):
-	_int_val(int_val), _string_val(string_val){}
+Foo::~Foo(){
+	delete[] _string_val;
+	_string_val = nullptr;
+}
 
 
 void Foo::set_int_val(int val) {
@@ -17,11 +25,17 @@ int Foo::get_int_val() {
 }
 
 
-void Foo::set_string_val(string val) {
-	_string_val = val;
+void Foo::set_string_val(char* val) {
+	if (_string_val != nullptr) {
+		delete[] _string_val;
+	}
+
+	auto count = strlen(val) + 1;
+	_string_val = new char[count];
+	strcpy(_string_val, val);
 }
 
 
-string Foo::get_string_val() {
+char* Foo::get_string_val() {
 	return _string_val;
 }
