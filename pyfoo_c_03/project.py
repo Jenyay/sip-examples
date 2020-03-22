7cbf6266242be2bdc06caac193048868c3f3e448
Eugene Ilin <jenyay.ilin@gmail.com>
2020-03-22 20:56:44 +0300

    Update project.py for examples

--- pyfoo_c_03/project.py
+++ pyfoo_c_03/project.py
@@ -32,17 +32,22 @@ class FooProject(Project):
     def get_options(self):
         """ Return the sequence of configurable options. """
 
+        tools = ['build', 'install', 'sdist', 'wheel']
+
         # Get the standard options.
         options = super().get_options()
 
         # Add our new options.
         inc_dir_option = Option('foo_include_dir',
-                                help="the directory containing foo.h", metavar="DIR")
+                                help="the directory containing foo.h",
+                                metavar="DIR",
+                                tools=tools)
         options.append(inc_dir_option)
 
         lib_dir_option = Option('foo_library_dir',
                                 help="the directory containing the foo library",
-                                metavar="DIR")
+                                metavar="DIR",
+                                tools=tools)
         options.append(lib_dir_option)
 
         return options
@@ -68,7 +73,7 @@ class FooProject(Project):
     def update(self, tool):
         """ Update the project configuration. """
 
-        # Get the foo bindings object.
+        # Get the pyfoo bindings object.
         foo_bindings = self.bindings['pyfoo']
 
         # Use any user supplied include directory.
@@ -78,3 +83,5 @@ class FooProject(Project):
         # Use any user supplied library directory.
         if self.foo_library_dir is not None:
             foo_bindings.library_dirs = [self.foo_library_dir]
+
+        super().update(tool)
--- pyfoo_cpp_02/project.py
+++ pyfoo_cpp_02/project.py
@@ -32,17 +32,22 @@ class FooProject(Project):
     def get_options(self):
         """ Return the sequence of configurable options. """
 
+        tools = ['build', 'install', 'sdist', 'wheel']
+
         # Get the standard options.
         options = super().get_options()
 
         # Add our new options.
         inc_dir_option = Option('foo_include_dir',
-                                help="the directory containing foo.h", metavar="DIR")
+                                help="the directory containing foo.h",
+                                metavar="DIR",
+                                tools=tools)
         options.append(inc_dir_option)
 
         lib_dir_option = Option('foo_library_dir',
                                 help="the directory containing the foo library",
-                                metavar="DIR")
+                                metavar="DIR",
+                                tools=tools)
         options.append(lib_dir_option)
 
         return options
@@ -78,3 +83,5 @@ class FooProject(Project):
         # Use any user supplied library directory.
         if self.foo_library_dir is not None:
             foo_bindings.library_dirs = [self.foo_library_dir]
+
+        super().update(tool)
